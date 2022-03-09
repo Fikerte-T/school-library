545a72d3b28caed304dc99b621a026943214c555
Fikerte-T <tofikertesfaye@gmail.com>
2022-03-09 11:22:58 +0300

    Update student and teacher constructors to extend all parent's instance variables

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,6 @@
 require './person'
 class Teacher < Person
-  def initialize(specialization)
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @specialization = specialization
   end
