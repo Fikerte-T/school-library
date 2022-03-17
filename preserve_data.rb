54735e93bd478decb4e68dbd07b0a39bc1af0a5d
Fikerte T <85518343+Fikerte-T@users.noreply.github.com>
2022-03-17 13:53:43 +0300

    Update preserve data class

--- preserve_data.rb
+++ preserve_data.rb
@@ -1,10 +1,5 @@
 require 'json'
-
 class PreserveData
-    # def initialize(file_name, data) 
-    #     @file_name = file_name
-    #     @data = data
-    # end
     attr_accessor :json_data
     def initialize
         @json_data = []
@@ -12,14 +7,15 @@ class PreserveData
     def write_to_file(file_name, data)
         file_data = File.read(file_name)
         if  file_data.empty?
+            @json_data.push(data)
             File.open(file_name,"w"){ |f| f.puts data.to_json}
         else 
             parsed_data = JSON.parse(file_data)
+            parsed_data.push(data)
             puts "parse_data"
-            @json_data.push(parsed_data)
-            @json_data.push(data)
-            File.truncate(file_name, 0)
-            File.open(file_name,"w"){ |f| f.puts json_data.to_json}
+            @json_data.clear
+            parsed_data.each { |d| @json_data.push(d)}
+            File.open(file_name,"w"){ |f| f.puts @json_data.to_json}
            
         end
     end
@@ -30,6 +26,7 @@ class PreserveData
             1
         else
             parsed_data = JSON.parse(file_data)
+            parsed_data
         end
     end
 end
