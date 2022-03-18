1f132f4478f39f949a73e9b2c6b61522326062e7
Fikerte-T <tofikertesfaye@gmail.com>
2022-03-18 12:39:29 +0300

    Add unit test for classroom

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -6,5 +6,15 @@ describe Classroom do
       classroom = Classroom.new('Class 1')
       expect(classroom).to be_an_instance_of Classroom
     end
+    it 'Classroom object has label of type string' do
+        classroom = Classroom.new('Class 1')
+        expect(classroom.label).to be_a(String)
+        expect(classroom.label).to eq ('Class 1')
+    end
+    it 'Classroom object responds to a method' do
+        classroom = Classroom.new('Class 1')
+        expect(classroom).to respond_to(:add_student)
+    end
+    
   end
 end
