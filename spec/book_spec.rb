2701a8b4c54c85ae4c695d6e7b474ef3ab914bf2
Fikerte-T <tofikertesfaye@gmail.com>
2022-03-18 12:34:36 +0300

    Add unit test for book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -6,5 +6,16 @@ describe Book do
       book = Book.new('The Hobbit', 'J.R.R. Tolkien')
       expect(book).to be_an_instance_of Book
     end
-  end
+    it 'Book object has title of type string' do
+        book = Book.new('The Hobbit', 'J.R.R. Tolkien')
+        expect(book.title).to be_a(String)
+        expect(book.title).to eq ('The Hobbit')
+    end
+    it 'Book object has author of type string' do
+        book = Book.new('The Hobbit', 'J.R.R. Tolkien')
+        expect(book.author).to be_a(String)
+        expect(book.author).to eq ('J.R.R. Tolkien')
+    end
+ 
+end
 end
